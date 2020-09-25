0a1c5a7779679e2cb81e39e9c9b0db772ee75b7b
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2020-09-25 21:25:38 +0500

    display the first 10 natural numbers

---
+++ Exercise_28.c
@@ -0,0 +1,17 @@
+//Write a program in C to display the first 10 natural numbers.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+
+    for(int i = 1; i<=10;i++)
+    {
+        printf("%d\t",i);
+    }
+
+
+    return 0;
+    getch();
+
+}
